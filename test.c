37402f736d242168526ff825c17d92e5f4548226
hemantmehra <mehra.hemant14@gmail.com>
2020-02-11 19:56:46 +0530

    Add test case message

--- test.c
+++ test.c
@@ -2,8 +2,18 @@
 #include <stdio.h>
 #include "writer.h"
 
+void test_start(char *s) {
+	printf("[Test Start] %s\n", s);
+}
+
+void test_end(char *s) {
+	printf("[Test End] %s\n", s);
+}
+
 void get_datatype_test() {
+	test_start("get_datatype()");
 	assert(get_datatype("i") == 4);
+	test_start("get_datatype()");
 }
 
 int main(int argc, char const *argv[])
